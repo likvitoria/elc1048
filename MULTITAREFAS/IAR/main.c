9839067fd1ef05e4cd9107f6b4a63ceac6e6d6fa
likvitoria <vick1610@hotmail.com>
2021-08-03 14:49:12 -0300

    Contador 5 threads

--- MULTITAREFAS/IAR/main.c
+++ MULTITAREFAS/IAR/main.c
@@ -7,29 +7,38 @@
 /*
  * Prototipos das tarefas
  */
-void produtor(void);
-void consumidor(void);
+void thread1(void);
+void thread2(void);
+void thread3(void);
+void thread4(void);
+void thread5(void);
+void fim(void);
 
+uint32_t count1 = 0, count2 =0, count3 = 0, count4 = 0, count5 = 0, soma = 0;
 
 /*
  * Configuracao dos tamanhos das pilhas
  */
 #define TAM_PILHA_1		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_3		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_4		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_5		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_6     (TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
  * Declaracao das pilhas das tarefas
  */
-uint32_t PILHA_PRODUTOR[TAM_PILHA_1];
-uint32_t PILHA_CONSUMIDOR[TAM_PILHA_2];
+uint32_t PILHA_1[TAM_PILHA_1];
+uint32_t PILHA_2[TAM_PILHA_2];
+uint32_t PILHA_3[TAM_PILHA_3];
+uint32_t PILHA_4[TAM_PILHA_4];
+uint32_t PILHA_5[TAM_PILHA_5];
+uint32_t PILHA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 
-semaforo_t cheio = {0, NULL};
-semaforo_t vazio = {TAM_BUFFER, NULL};
-int buffer[TAM_BUFFER];
-int i = 0, j =  0, valor;
 /*
  * Funcao principal de entrada do sistema
  */
@@ -39,14 +48,28 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(produtor, "Produtor", PILHA_PRODUTOR, TAM_PILHA_1, 1);
+	CriaTarefa(thread1, "Thread 1", PILHA_1, TAM_PILHA_1, 5);
+
+	CriaTarefa(thread2, "Thread 2", PILHA_2, TAM_PILHA_2, 4);
+
+	CriaTarefa(thread3, "Thread 3", PILHA_3, TAM_PILHA_3, 3);
+
+	CriaTarefa(thread4, "Thread 4", PILHA_4, TAM_PILHA_4, 2);
 	
-	CriaTarefa(consumidor, "Consumidor", PILHA_CONSUMIDOR, TAM_PILHA_2, 2);
+	CriaTarefa(thread5, "Thread 5", PILHA_5, TAM_PILHA_5, 1);
+
+	CriaTarefa(fim, "Soma", PILHA_6, TAM_PILHA_6, 6);
 
 
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
 	
+	 	    TCB[1].estado = ESPERA;
+	        TCB[2].estado = ESPERA;
+	        TCB[3].estado = ESPERA;
+	        TCB[4].estado = ESPERA;
+	        TCB[5].estado = ESPERA;
+
 	/* Configura marca de tempo */
 	ConfiguraMarcaTempo();   
 	
@@ -63,27 +86,54 @@ int main(void)
 
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
 
-void produtor(void){
+void thread5(void){
 
-	while(1){
-		SemaforoAguarda(&vazio);
-		buffer[i]=rand()%20;	//produz();
-
-		i = (i+1)%TAM_BUFFER;
-		SemaforoLibera(&cheio);
-
+	for(;;){
+		count5++;
+		TarefaContinua(4);
 	}
 }
 
-void consumidor(void){
+void thread4(void){
 
-	while(1){
-		SemaforoAguarda(&cheio);
-		valor = buffer[j]; //consome
+	for(;;){
+		count4++;
+		TarefaContinua(3);
+		TarefaSuspende(4);
+	}
+}
 
-		j = (j+1)%TAM_BUFFER;
-		SemaforoLibera(&vazio);
+void thread3(void){
 
+	for(;;){
+		count3++;
+		TarefaContinua(2);
+		TarefaSuspende(3);
+	}
+}
+
+void thread2(void){
+
+	for(;;){
+		count2++;
+		 TarefaContinua(1);
+		 TarefaSuspende(2);
+	}
+}
+
+void thread1(void){
+
+	for(;;){
+		count1++;
+		 TarefaSuspende(1);
+	}
+}
+
+void fim(void){
+
+	for(;;){
+		soma = count1 + count2 + count3 + count4 + count5;
+		TarefaEspera(30);
 	}
 }
 
--- MULTITAREFAS/IAR/multitarefas.h
+++ MULTITAREFAS/IAR/multitarefas.h
@@ -14,10 +14,10 @@
 /* macros de configuracao */
 
 /* numero de tarefas */
-#define NUMERO_DE_TAREFAS	3
+#define NUMERO_DE_TAREFAS	7
 
 /* n�mero de prioridades/tarefas */
-#define PRIORIDADE_MAXIMA   2
+#define PRIORIDADE_MAXIMA   6
 
 /* frequencia de clock da CPU */
 #define cfg_CPU_CLOCK_HZ 	48000000
